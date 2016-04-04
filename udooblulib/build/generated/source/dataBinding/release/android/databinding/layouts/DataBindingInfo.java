a041a205344ce3d63698feacd31cf7e41116e98a
harlem88 <harlem88.ant@gmail.com>
2016-04-04 17:47:51 +0200

    add udooBlu example

---
+++ udooblulib/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="79ff0df0-ac7d-431a-a4f9-9a083b1b2dcb", modulePackage="org.udoo.udooblulib", sdkRoot="/home/harlem88/android/sdk",layoutInfoDir="/home/harlem88/work/udoo/workspace/UDOOBluLib/udooblulib/build/intermediates/data-binding-info/release",exportClassListTo="/home/harlem88/work/udoo/workspace/UDOOBluLib/udooblulib/build/intermediates/data-binding-info/release/_generated.txt",isLibrary=true,minSdk=21,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
---
+++ udooblulib/build/intermediates/classes/debug/org/udoo/udooblulib/BR.java
@@ -0,0 +1,5 @@
+package org.udoo.udooblulib;
+
+public class BR {
+        public static int _all = 0;
+}
